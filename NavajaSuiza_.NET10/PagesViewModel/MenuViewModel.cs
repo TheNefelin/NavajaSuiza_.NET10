b666931b2f66e35ae7929c608a9b0050ab74f81a
Francisco Carmona <flcarmonac@yahoo.com>
2026-01-03 16:28:00 -0300

    Update: Theme

--- NavajaSuiza_.NET10/PagesViewModel/MenuViewModel.cs
+++ NavajaSuiza_.NET10/PagesViewModel/MenuViewModel.cs
@@ -1,13 +1,21 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using NavajaSuiza_.NET10.Pages;
 
 namespace NavajaSuiza_.NET10.PagesViewModel;
 
 public partial class MenuViewModel : BaseViewModel
 {
+    private readonly IServiceProvider _serviceProvider;
+
+    public MenuViewModel(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
     [RelayCommand]
     private async Task NavigateToFlashlight()
     {
-        //await AppShell.DisplayToastAsync("All cleaned up!");
-        await Shell.Current.GoToAsync("//flashlight");
+        var page = _serviceProvider.GetRequiredService<FlashlightPage>();
+        await Shell.Current.Navigation.PushAsync(page);
     }
 }
